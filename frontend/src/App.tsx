aa1d173acc51bcec95b586e0dfab5fa0e2e409c8
InNUenD00 <ernesto.torrez.01@est.ulsa.edu.ni>
2026-03-12 20:02:31 -0600

    typography changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import './index.css'
 
 function CompanyCard({ name }: { name: string }) {
   return (
