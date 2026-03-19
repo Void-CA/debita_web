65215fe7876ed93d154e644238825c8c46d8f45b
InNUenD00 <ernesto.torrez.01@est.ulsa.edu.ni>
2026-03-19 16:40:06 -0600

    Regreso del Botón agregar cliente y cambios de dimension en CompanyCard.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -52,27 +52,35 @@ function App() {
       {/* MAIN CONTENT */}
       <main className="flex-1 flex flex-col p-8 overflow-y-auto bg-teal-100/80">
         
-        <header className="flex justify-between items-center mb-10 w-500 px-100">
-          <h2 className="text-4xl font-poppins font-semibold text-white">Gestión de Finanzas Operativa</h2>
+        <header className="flex justify-between items-center mb-10 w-310 px-10">
+
+          <h2 className="flex flex-row text-4xl font-poppins font-semibold text-white">Gestión de Finanzas Operativa
+          </h2>
+           
+          {/* Botón de crear*/}
+          <button className="flex flex-row items-center px-4 py-2 rounded-lg font-poppins font-semibold bg-blue-500 hover:bg-blue-600 text-white hover:-translate-y-1 hover:shadow-md hover:shadow-black transition-all duration-200 cursor-pointer">
+            + Agregar Cliente
+          </button>
+
         </header>
 
         {/* Contenedor padre para separar las tarjetas */}
         <div className="flex flex-row gap-10 ml-10">
 
-          {/*Tarjeta de Clientes*/}
-          <div className="flex flex-col items-center justify-center w-56 h-52 bg-[#32b569] shadow-md rounded-3xl cursor-pointer hover:-translate-y-1 hover:shadow-lg hover:shadow-black transition-all duration-200">
+          {/*Tarjeta de Cliente eje*/}
+          <div className="flex flex-col items-center justify-center w-70 h-50 bg-[#32b569] shadow-md rounded-3xl cursor-pointer hover:-translate-y-1 hover:shadow-lg hover:shadow-black transition-all duration-200">
             <p className="text-black font-semibold font-poppins text-xl">Empresa 1</p>
-          </div>
         
-        </div>
-        {/* Sección de Tarjetas */}
-        <section className="flex flex-wrap gap-6 w-full mt-12 ml-10">
-          {loading && <p>Cargando clientes...</p>}
+          </div>
+          {/* Sección de Tarjetas */}
+          <section className="flex flex-wrap gap-6 w-full mt-12 ml-10">
+           {loading && <p>Cargando clientes...</p>}
 
-          {companies?.map(c => (
-            <CompanyCard key={c.name} company_name={c.name} />
-          ))}
-        </section>
+            {companies?.map(c => (
+             <CompanyCard key={c.name} company_name={c.name} />
+           ))}
+          </section>
+        </div>
       </main>
       
     </div>
--- frontend/src/features/companies/CompanyCard.tsx
+++ frontend/src/features/companies/CompanyCard.tsx
@@ -1,6 +1,6 @@
 export default function CompanyCard({ company_name }: { company_name: string }) {
   return (
-    <div className="flex items-center justify-center font-sketch font-semibold w-60 h-45 bg-white border-3 border-black rounded-2xl cursor-pointer hover:-translate-y-1 hover:shadow-lg hover:shadow-black transition-all duration-200 text-xl ">
+    <div className="flex flex-col items-center justify-center w-70 h-50 bg-[#32b569] shadow-md rounded-3xl cursor-pointer hover:-translate-y-1 hover:shadow-lg hover:shadow-black transition-all duration-200">
       <p>{company_name}</p>
     </div>
   )
